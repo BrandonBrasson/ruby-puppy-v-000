727e19bfeea779f4c66cd42f523645847edd4743
Brandon Brasson <rocksong622@gmail.com>
2019-01-11 21:22:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,15 @@
 class Dog
+
 @@all = []
 
     def initialize(name)
       @name = name
       @@all << self
     end
+def self.pets(name)
+pets = name
+name = []
+end
 
     def self.all
       puts @@all.map{ |dog| dog.name }
